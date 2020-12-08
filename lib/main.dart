f6604cf3faa9caf9307483584dd7950f51538e44
Ellen Hafström <hafstrom.ellen@gmail.com>
2020-12-08 13:10:39 +0100

    test

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  
+}
+
+class MainView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      )
+    );
+  }
+}
