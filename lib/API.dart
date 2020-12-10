c470cda9e0fbb905db97d654765ed14971779437
lovisaakerstrom <lollo_345@hotmail.com>
2020-12-10 16:49:43 +0100

    searchBar

---
+++ lib/API.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'model.dart';
+
+const API_URL = 'https://api.spoonacular.com/';
+const API_KEY = '65280183935d450689c08febc1911c10';
+
+class API {
+  static Future<List<Recipe>> getRecipe() async {
+    var response =
+        await http.get('$API_URL/recipes/complexSearch?key=$API_KEY');
+    print(response.body);
+    var json = jsonDecode(response.body);
+    print(json);
+
+    return json.map<Recipe>((data) {
+      return Recipe.fromJson(data);
+    }).toList();
+  }
+}
--- lib/model.dart
+++ lib/model.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/cupertino.dart';
 
-class Recipe{
+import 'API.dart';
+
+class Recipe {
   String title;
 
+  Recipe({
+    this.title,
+  });
 
-Recipe({
-this.title,
-});
+  static Map<String, dynamic> toJson(Recipe recipe) {
+    return {
+      'query': recipe.title,
+    };
+  }
+
+  static Recipe fromJson(Map<String, dynamic> json) {
+    return Recipe(title: json['query']);
+  }
 }
 
 class MyState extends ChangeNotifier {
   List<Recipe> _list = [];
   List<Recipe> get list => _list;
 
-void removeRecipe(int index) {
-_list.remove(index);
-notifyListeners();
-}
+  void removeRecipe(int index) {
+    _list.remove(index);
+    notifyListeners();
+  }
 
   String _filterList = 'Visa alla';
   String get filterList => _filterList;
@@ -24,5 +35,11 @@ notifyListeners();
   void setFilter(String filterList) {
     this._filterList = filterList;
     notifyListeners();
-}
+  }
+
+  Future getRecipe() async {
+    List<Recipe> list = await API.getRecipe();
+    _list = list;
+    notifyListeners();
+  }
 }
