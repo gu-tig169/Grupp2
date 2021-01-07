4be38464effbdcf44a980b28eed80bb32733c2a2
tildamosseen <gusmosti@student.gu.se>
2021-01-07 13:55:37 +0100

    formatering

--- lib/grocerySearchList.dart
+++ lib/grocerySearchList.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe/models/model.dart';
-import 'buildPopUpDialog.dart';
-
 
 class GrocerySearchList extends StatelessWidget {
   final RecipeInformation grocery;
@@ -24,26 +22,32 @@ class GrocerySearchList extends StatelessWidget {
                       child: Padding(
                           padding: EdgeInsets.all(5.0),
                           child: ListTile(
-                            title: Text(results[index].name,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                )),
-                            trailing: IconButton(
-                                icon: Icon(Icons.add),
-                                color: Colors.grey,
-                                onPressed: () {
-                                  var state = Provider.of<MyState>(context,
-                                      listen: false);
-                                  state.addGrocery(grocery.ingredients[index]);
-                                  showDialog(context: context,
-          builder: (BuildContext context) => 
-          BuildPopupDialog(),
-          );
-                                }),
-                          )))));
+                              title: Text(results[index].name,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  )),
+                              trailing: IconButton(
+                                  icon: Icon(Icons.add),
+                                  color: Colors.grey,
+                                  onPressed: () {
+                                    var state = Provider.of<MyState>(context,
+                                        listen: false);
+                                    state
+                                        .addGrocery(grocery.ingredients[index]);
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          Future.delayed(Duration(seconds: 2),
+                                              () {
+                                            Navigator.of(context).pop(true);
+                                          });
+                                          return new AlertDialog(
+                                              title: const Text(
+                                                  'This grocery has been added to your shoppinglist'),
+                                              backgroundColor: Colors.white
+                                                  .withOpacity(0.8));
+                                        });
+                                  }))))));
         });
   }
 }
-    
-
-
